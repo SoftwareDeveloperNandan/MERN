cc24f5839975932c214f9586eba1badc02dcdb48
SoftwareDeveloperNandan <nandanpanditsd@gmail.com>
2024-03-23 16:37:15 +0530

    push

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "Chai aur Code......."
-    })
+//    get user details from frontend
+// get firstname, password, email, Username
+// validation all details from frontend
+// check if user already exist or not
+//check for images, check for
 })
 
 export {registerUser}
